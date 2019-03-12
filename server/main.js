3eb17f624c5d9172fa5ff51c030079fdc1bc484f
michael.landry <michael.landry@fiserv.com>
2019-03-12 12:29:41 -0400

    apollo in

--- server/main.js
+++ server/main.js
@@ -1,31 +1,40 @@
 import { Meteor } from 'meteor/meteor';
 import Links from '/imports/api/links';
+import { makeExecutableSchema } from "graphql-tools"
+import { ApolloServer, gql } from 'apollo-server-express'
+import { WebApp } from 'meteor/webapp'
+import { getUser } from 'meteor/apollo'
 
-function insertLink(title, url) {
-  Links.insert({ title, url, createdAt: new Date() });
+// import typeDefs from './schema'
+// import resolvers from './resolvers'
+const typeDefs = `
+type Query {
+  hi: String 
 }
+`
+const resolvers = () => { }
+const server = new ApolloServer({
+  typeDefs,
+  // resolvers,
+  // context: async ({ req }) => ({
+  //   user: await getUser(req.headers.authorization)
+  // })
+})
+
+server.applyMiddleware({
+  app: WebApp.connectHandlers,
+  path: '/graphql'
+})
+
+WebApp.connectHandlers.use('/graphql', (req, res) => {
+  if (req.method === 'GET') {
+    res.end()
+  }
+})
+
+console.log('in main.js');
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (Links.find().count() === 0) {
-    insertLink(
-      'Do the Tutorial',
-      'https://www.meteor.com/tutorials/react/creating-an-app'
-    );
+  console.log('Meteor starting up...');
 
-    insertLink(
-      'Follow the Guide',
-      'http://guide.meteor.com'
-    );
-
-    insertLink(
-      'Read the Docs',
-      'https://docs.meteor.com'
-    );
-
-    insertLink(
-      'Discussions',
-      'https://forums.meteor.com'
-    );
-  }
 });
