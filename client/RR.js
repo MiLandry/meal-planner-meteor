eb92b5cf9aeb724c62b49db3bf8d0b9d8c2161c4
michael.landry <michael.landry@fiserv.com>
2019-03-14 15:49:45 -0400

    home route

--- client/RR.js
+++ client/RR.js
@@ -2,12 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router';
 import createBrowserHistory from 'history/createBrowserHistory';
 
-// route components
-// import AppContainer from '../../ui/containers/AppContainer.js';
-// import ListPageContainer from '../../ui/containers/ListPageContainer.js';
-// import AuthPageSignIn from '../../ui/pages/AuthPageSignIn.js';
-// import AuthPageJoin from '../../ui/pages/AuthPageJoin.js';
-// import NotFoundPage from '../../ui/pages/NotFoundPage.js';
+import App from '../imports/ui/App'
 
 const TestComponent = () => (<h1>test</h1>)
 
@@ -16,7 +11,7 @@ const browserHistory = createBrowserHistory();
 const renderRoutes = () => (
   <Router history={browserHistory}>
     <Switch>
-      {/* <Route exact path="/" component={AppContainer}/> */}
+      <Route exact path="/" component={App} />
       {/* <Route exact path="/lists/:id" component={ListPageContainer}/> */}
       <Route exact path="/test" component={TestComponent} />
       {/* <Route exact path="/join" component={AuthPageJoin}/> */}
